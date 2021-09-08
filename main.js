ed85625226ed8e3b02b820c05329bc8a007a0078
Lukasz <wac.lukasz@gmail.com>
2021-09-08 20:29:47 +0200

    feat: improves data model

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const setDate = () => {
     const dateDayElement = document.querySelector('.date-day');
+    const monthNameElement = document.getElementById('monthName');
+    const yearElement = document.querySelector('.date-month-year > span:last-of-type');
+
     let dayOfWeek = new Date().getDate();
+    const year = new Date().getFullYear();
+    const monthName = new Date().toLocaleDateString('en-en', { month: 'short' });
 
     if (dayOfWeek < 10) {
         dayOfWeek = `0${dayOfWeek}`;
     };
     
     dateDayElement.innerText = dayOfWeek;
+    yearElement.innerText = year;
+    monthNameElement.innerText = monthName;
 }
 
-
-console.log(dateDayElement);
+setDate();
