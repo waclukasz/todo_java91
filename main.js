7bdfa029687aa1f4605d4b2802d10ada1f1dcb6b
Lukasz <wac.lukasz@gmail.com>
2021-09-08 20:13:04 +0200

    feat: adds dates

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+const setDate = () => {
+    const dateDayElement = document.querySelector('.date-day');
+    let dayOfWeek = new Date().getDate();
+
+    if (dayOfWeek < 10) {
+        dayOfWeek = `0${dayOfWeek}`;
+    };
+    
+    dateDayElement.innerText = dayOfWeek;
+}
+
+
+console.log(dateDayElement);
