fa928b26f0ee9a230180dbb580da6f47e7f0be7f
Lukasz <wac.lukasz@gmail.com>
2021-09-09 20:09:25 +0200

    feat: add task component

--- main.js
+++ main.js
@@ -33,3 +33,69 @@ displayModalBtnElement.addEventListener('click', toggleModal);
 
 const closeBtnElement = document.querySelector('.close-modal-btn');
 closeBtnElement.addEventListener('click', toggleModal);
+
+const renderTask = () => {
+    // get element from HTML
+    const taskListElement = document.querySelector('.tasks-list');
+
+    // create element in JS
+    const taskContainer = document.createElement('div');
+    const taskParagraph = document.createElement('p');
+    const checkBtn = document.createElement('button');
+    const checkIcon = document.createElement('i');
+
+    taskContainer.classList.add('task-container');
+    checkBtn.classList.add('check-btn');
+    checkIcon.classList.add('fas', 'fa-check');
+
+    taskParagraph.innerText = 'My first'
+
+    // created elements added to HTML
+    taskContainer.appendChild(taskParagraph);
+    taskContainer.appendChild(checkBtn);
+    checkBtn.appendChild(checkIcon)
+    taskListElement.appendChild(taskContainer);
+}
+
+const validateTask = (taskName) => {
+    if (!taskName.trim()) {
+        return 'Task name is required'
+    }
+
+    if (taskName.length < 4) {
+        return 'Min 3 characters'
+    }
+
+    return;
+}
+
+const addTaskToModel = (taskInput) => {
+    const task = {
+        name: taskInput.value,
+        isCompleted: false,
+        id: Math.random().toString()
+    };
+
+    tasksList.push(task);
+
+    taskInput.value = '';
+}
+
+
+const addTaskBtnElement = document.getElementById('addTask');
+addTaskBtnElement.addEventListener('click', () => {
+    const taskNameInputElement = document.getElementById('taskName');
+    
+    if (validateTask(taskNameInputElement.value)) {
+        alert(validateTask(taskNameInputElement.value))
+        return;
+    }
+
+    addTaskToModel(taskNameInputElement);
+    toggleModal();
+})
+
+
+
+
+
